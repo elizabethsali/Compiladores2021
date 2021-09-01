2b8f2ad4d49527da561d82b64d7f301116809487
elizabethsali <81321891+elizabethsali@users.noreply.github.com>
2021-08-31 19:04:38 -0500

    Se agrega clase AFN

--- Practica2/functions.cpp
+++ Practica2/functions.cpp
@@ -18,7 +18,7 @@ void generateAFN(char * regularExpresion){
 	int id = 0, idC = 0;
 	int i = 0;
 
-	for(i = 0; i < length; i++){
+	/*for(i = 0; i < length; i++){
 		if(isTransition(regularExpresion[i]) == 1){
 			characters[idC] = regularExpresion[i];
 			estadosIniciales[idC] = id;
@@ -27,6 +27,19 @@ void generateAFN(char * regularExpresion){
 			idC++;
 			id++;
 		}
+	}	*/
+
+	AFN automata;
+	automata.estadoActual = 0;
+	automata.estadoInicial = 0;
+	automata.estadoFinal = 0;
+	automata.id = 0;
+	automata.posicion = 0;
+
+
+	for(i = 0; i < length; i++){
+		printf("Estado actual: %d\n", automata.estadoActual);
+		automata = transicion(automata,regularExpresion[i]);
 	}	
 
 
@@ -65,31 +78,79 @@ int isTransition(char c){
 	return 0;
 }
 
-void readRegularExpresion(char * regularExpresion, int length){
-	int i = 0;
+AFN transicion(AFN automata,char caracter){
 
-	for(i = 0; i < length; i++){
-		if(regularExpresion[i] == '('){
-			readParentesis(regularExpresion, length, i + 1);
-		}else if(regularExpresion[i] == ')'){
-			
-		}
+	int i = 0, inicialUnion = 0;
 
+	if(caracter == '('){
+		automata.estadoActual = 1;
+	}else if(isTransition(caracter) == 1){
+		switch(automata.estadoActual){
+			case 0:
+				automata.characters[automata.posicion] = caracter;
+				automata.estadosIniciales[automata.posicion] = automata.id;
+				automata.id++;
+				automata.estadosFinales[automata.posicion] = automata.id;
+				automata.posicion++;
+				automata.estadoActual = 2;
+				break;
+			case 1:
+				automata.estadoActual = 2;
+				break;
+			case 2:
+				automata.characters[automata.posicion] = caracter;
+				automata.estadosIniciales[automata.posicion] = estadosFinales[automata.posicion - 1];
+				automata.id++;
+				automata.estadosFinales[automata.posicion] = automata.id;
+				automata.posicion++;
+				automata.estadoActual = 2;
+				break;
+			case 3:
+				automata.characters[automata.posicion] = caracter;
+				automata.id++;
+				inicialUnion = automata.id;
+				automata.estadosIniciales[automata.posicion] = automata.id;
+				automata.id++;
+				automata.estadosFinales[automata.posicion] = automata.id;
+				automata.posicion++;
+
+				automata.id++;
+				AFN automataNew;
+				automataNew.estadoActual = 2;
+				automataNew.estadoInicial = automata.id;
+				automataNew.estadoFinal = automata.id;
+				automataNew.id = automata.id;
+				automataNew.posicion = 0;
+
+				automataNew.characters[automataNew.posicion] = 'E';
+				automataNew.estadosIniciales[automataNew.posicion] = automataNew.id;
+				automataNew.estadosFinales[automataNew.posicion] = automata.estadosIniciales[0];
+				automataNew.posicion++;
+
+				automataNew.characters[automataNew.posicion] = 'E';
+				automataNew.estadosIniciales[automataNew.posicion] = automataNew.id;
+				automataNew.id++;
+				automataNew.estadosFinales[automataNew.posicion] = inicialUnion;
+				automataNew.posicion++;
+
+				i = 0;
+
+				/*for(i = 0; i < automata.posicion; i++){
+
+				}*/
+
+				break;
+			case 4:
+				automataNew.estadoActual = 2;
+				break;
+		}		
+	}else if(caracter == '|'){
+		automata.estadoActual = 3;
+	}else if(caracter == ')'){
+
+		automata.estadoActual = 4;
 	}
+
+
+	return automata;
 }
-
-int readParentesis(char * regularExpresion, int length, int pos){
-	int i = 0;
-
-	for(i = pos; i < length; i++){
-		if(regularExpresion[i] == '('){
-			readParentesis(regularExpresion, length, i + 1);
-		}else if(regularExpresion[i] == ')'){
-			return 0;
-		}
-
-	}
-
-	return 0;
-}
-
--- Practica2/functions.h
+++ Practica2/functions.h
@@ -1,4 +1,36 @@
+class AFN
+{
+public:
+    int estadoInicial;
+    int estadoActual;
+    int estadoFinal;
+    char caracter;
+
+	char characters[100];
+	int estadosIniciales[100];
+	int estadosFinales[100];
+	int id;
+	int posicion;
+
+    //int transiciones[][];
+    
+    /*char* alfabeto;
+    char* palabra;
+    
+    void leerPalabra( char* palabra );
+    
+    void transicion(char caracter);
+    
+    void setEstado(int e);
+    int getEstado();
+    void setEstadoActual(int e);
+    int getEstadoActual();
+    void setEstadoFinal(int e);
+    int getEstadoFinal();*/
+};
+
 void generateAFN(char * regularExpresion);
 int isTransition(char c);
 void readRegularExpresion(char * regularExpresion, int length);
 int readParentesis(char * regularExpresion, int length, int pos);
+AFN transicion(AFN automata,char caracter);
